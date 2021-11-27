2a8ed6b74012799c26d0511bb14eeb2aa1a66bf9
Khawaja Samran <moon_samran@hotmail.com>
2021-11-27 12:57:57 +0100

    Tests Project added

---
+++ TennisGameTests/GameScorerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TennisScoring;
+
+namespace TennisGameTets
+{
+    [TestClass]
+    public class GameScorerTests
+    {
+        [TestMethod]
+        public void UpdateGameScore_Player1Won_Player1GameScoreIsEqualToOne()
+        {
+            //Arrange
+            IPlayer player1 = new Player();
+            IPlayer player2 = new Player();
+            IGameScorer gameScorer = new GameScorer();
+
+            // Act
+            gameScorer.UpdateGameScore(1,player1,player2);
+
+            //Assert
+            Assert.AreEqual(1, player1.GetPointsPerGame());
+        }
+        [TestMethod]
+        public void UpdateGameScore_Player2Won_Player2GameScoreIsEqualToOne()
+        {
+            //Arrange
+            IPlayer player1 = new Player();
+            IPlayer player2 = new Player();
+            IGameScorer gameScorer = new GameScorer();
+
+            // Act
+            gameScorer.UpdateGameScore(2, player1, player2);
+
+            //Assert
+            Assert.AreEqual(1, player2.GetPointsPerGame());
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Server out of course. Again serve")]
+        public void UpdateGameScore_NoPlayerWon_ExceptionOccurs()
+        {
+            //Arrange
+            IPlayer player1 = new Player();
+            IPlayer player2 = new Player();
+            IGameScorer gameScorer = new GameScorer();
+
+            // Act
+            gameScorer.UpdateGameScore(3, player1, player2);
+
+
+        }
+    }
+}
---
+++ TennisGameTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisGameTets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisGameTets")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("250c6224-40e6-4ea6-abf2-be5be3efa0a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
